aa5d3151b03dda1f5e71f44f9db2ed15af656db3
Valentine <simpletine2022@gmail.com>
2022-07-12 22:37:31 +0800

    Remove error document

--- mvc/app/Language/en/Mycustom.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'first' => 'today i have a cup of coffee',
-    'second' => 'I loves yellow color',
-    'third' => 'I bought a new cloth with simpletine logo',
-];
--- mvc/app/Views/login/login-form.php
+++
@@ -1,6 +0,0 @@
-<?= $login ?? '' ?>
-<?= form_open('home/login-validation') ?>
-<?= form_input('username', '', 'placeholder="Enter your username"'); ?>
-<?= form_input('password', '', 'placeholder="Enter your password"'); ?>
-<?= form_submit('login', 'Login button'); ?>
-<?= form_close() ?>
