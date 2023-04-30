de2a31f6903fab749bab051c68b1f12be6f36c67
Rajat Gupta <rajatinvoice@gmail.com>
2023-04-30 12:14:23 +0000

    Apr 30, 2023, 5:44 PM

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://simpletine-hmvc.com';
+	public $baseURL = 'http://simpletine-hmvc.com/';
 
 	/*
 	|--------------------------------------------------------------------------
